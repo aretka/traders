d4b3404ac281b25c92cfd978df179237bb6b7e99
aretka <aretask2@gmail.com>
2021-11-09 16:29:57 +0200

    Reformat code

--- app/src/main/java/com/example/traders/MainActivity.kt
+++ app/src/main/java/com/example/traders/MainActivity.kt
@@ -21,7 +21,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setUpBottomNavigation()
+    }
 
+    private fun setUpBottomNavigation() {
         val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottom_nav)
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
