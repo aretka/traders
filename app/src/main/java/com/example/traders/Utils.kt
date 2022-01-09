30974b003c45a8f199cf5e6fa4a6d26174b4bebc
aretka <aretask2@gmail.com>
2022-01-09 10:31:27 +0200

    Fix bug

--- app/src/main/java/com/example/traders/Utils.kt
+++ app/src/main/java/com/example/traders/Utils.kt
@@ -12,18 +12,20 @@ fun getCryptoPriceChangeText(
     percentagePriceChange: String,
     textView: TextView
 ) {
-
+    var finalPriceChange = priceChange
     if (percentagePriceChange.contains('-')) {
+        if(!priceChange.contains('-')) finalPriceChange = "-" + finalPriceChange
         textView.text = textView.context.getString(
             R.string.crypto_price_red,
-            priceChange,
+            finalPriceChange,
             percentagePriceChange
         )
         textView.setTextColor(ContextCompat.getColor(textView.context, R.color.red))
     } else {
+        if(priceChange.contains('-')) finalPriceChange = finalPriceChange.replace("-", "")
         textView.text = textView.context.getString(
             R.string.crypto_price_green,
-            priceChange,
+            finalPriceChange,
             percentagePriceChange
         )
         textView.setTextColor(ContextCompat.getColor(textView.context, R.color.green))
