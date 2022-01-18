ccb8d67c3b10cd110459637ea81e0382b14edfce
Nerius Zarnauskas <nerius.zarnauskas@vinted.com>
2022-01-18 12:04:36 +0200

    Integrate Flipper tool

--- app/src/main/java/com/example/traders/TradersApplication.kt
+++ app/src/main/java/com/example/traders/TradersApplication.kt
@@ -1,7 +1,15 @@
 package com.example.traders
 
 import android.app.Application
+import com.example.traders.debug.FlipperInitializer
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-open class TradersApplication : Application()
+open class TradersApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        FlipperInitializer.init(this)
+    }
+}
