81afb3ab47f6466d2f2e4707cf45420e47ee2476
aretka <aretask2@gmail.com>
2022-01-19 11:24:32 +0200

    Add diff utils to recycler view

--- app/src/main/java/com/example/traders/watchlist/adapters/WatchListAdapter.kt
+++ app/src/main/java/com/example/traders/watchlist/adapters/WatchListAdapter.kt
@@ -2,6 +2,8 @@ package com.example.traders.watchlist.adapters
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import androidx.viewbinding.ViewBinding
 import com.bumptech.glide.Glide
@@ -10,14 +12,11 @@ import com.example.traders.databinding.ListItemCryptoBinding
 import com.example.traders.getCryptoPriceChangeText
 import com.example.traders.roundNumber
 import com.example.traders.watchlist.cryptoData.FixedCryptoList
-import com.example.traders.watchlist.cryptoData.binance24HourData.Binance24Data
 import com.example.traders.watchlist.cryptoData.binance24HourData.Binance24DataItem
 import com.example.traders.watchlist.cryptoData.cryptoPriceData.Data
-import kotlin.math.round
 
 class WatchListAdapter(private val clickListener: SingleCryptoListener) :
-    RecyclerView.Adapter<SimpleViewHolder<ListItemCryptoBinding>>() {
-    private var list: List<Binance24DataItem> = emptyList()
+    ListAdapter<Binance24DataItem, SimpleViewHolder<ListItemCryptoBinding>>(DiffCallback()) {
 
     override fun onCreateViewHolder(
         parent: ViewGroup,
@@ -28,11 +27,16 @@ class WatchListAdapter(private val clickListener: SingleCryptoListener) :
         return SimpleViewHolder(ListItemCryptoBinding.inflate(layoutInflater, parent, false))
     }
 
+
     override fun onBindViewHolder(holder: SimpleViewHolder<ListItemCryptoBinding>, position: Int) {
-        val item = list[position]
+        val item = currentList[position]
+        val symbol = item.symbol.replace("USDT", "")
+        val slug = FixedCryptoList.valueOf(symbol).slug
+
+        holder.binding.root.setOnClickListener { clickListener.onClick(slug, symbol) }
         holder.binding.cryptoPrice.text = roundNumber(item.lastPrice.toDouble())
         holder.binding.cryptoNameShortcut.text = item.symbol
-        holder.binding.cryptoFullName.text = FixedCryptoList.valueOf(item.symbol.replace("USDT", "")).slug.replaceFirstChar { c -> c.uppercase() }
+        holder.binding.cryptoFullName.text = slug.replaceFirstChar { c -> c.uppercase() }
         getCryptoPriceChangeText(
             roundNumber(item.priceChange.toDouble()),
             roundNumber(item.priceChangePercent.toDouble()),
@@ -43,45 +47,20 @@ class WatchListAdapter(private val clickListener: SingleCryptoListener) :
             .placeholder(R.drawable.ic_download)
             .error(R.drawable.ic_image_error)
             .into(holder.binding.cryptoLogo)
-
-//        val priceChange = roundNumber(
-//            item.metrics.market_data.ohlcv_last_24_hour.close - item.metrics.market_data.ohlcv_last_24_hour.open
-//        )
-//
-//        val percentagePriceChange =
-//            roundNumber(item.metrics.market_data.percent_change_usd_last_24_hours)
-//
-//        holder.binding.root.setOnClickListener {
-//            clickListener.onClick(item)
-//        }
-//        holder.binding.cryptoNameShortcut.text = item.symbol
-//        holder.binding.cryptoFullName.text = item.slug?.replaceFirstChar { char -> char.uppercase() }
-//        holder.binding.cryptoPrice.text =
-//            roundNumber(item.metrics.market_data.ohlcv_last_24_hour.close)
-//        getCryptoPriceChangeText(
-//            priceChange,
-//            percentagePriceChange,
-//            holder.binding.cryptoPriceChange
-//        )
-//
-//        // Glide downloads and caches image in local storage for later usage
-//        Glide.with(holder.binding.cryptoLogo)
-//            .load("https://cryptologos.cc/logos/${item.slug}-${item.symbol?.lowercase()}-logo.png?v=014")
-//            .placeholder(R.drawable.ic_download)
-//            .error(R.drawable.ic_image_error)
-//            .into(holder.binding.cryptoLogo)
     }
 
-    override fun getItemCount(): Int = list.size
+    private class DiffCallback : DiffUtil.ItemCallback<Binance24DataItem>() {
 
-    fun updateData(list: List<Binance24DataItem>) {
-        this.list = list
-        this.notifyDataSetChanged()
+        override fun areItemsTheSame(oldItem: Binance24DataItem, newItem: Binance24DataItem) =
+            oldItem.symbol == newItem.symbol
+
+        override fun areContentsTheSame(oldItem: Binance24DataItem, newItem: Binance24DataItem) =
+            oldItem == newItem
     }
 }
 
 class SimpleViewHolder<T : ViewBinding>(val binding: T) : RecyclerView.ViewHolder(binding.root)
 
 class SingleCryptoListener(val clickListener: (id: String, symbol: String?) -> Unit) {
-    fun onClick(data: Data) = clickListener(data.id, data.symbol)
+    fun onClick(slug: String, symbol: String) = clickListener(slug, symbol)
 }
