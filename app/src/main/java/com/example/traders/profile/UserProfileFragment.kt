4e1dcb028a965c0ba263b9f8b96c0d2a3490c1da
aretka <aretask2@gmail.com>
2022-01-27 15:39:27 +0200

    Modify architecture

--- app/src/main/java/com/example/traders/profile/UserProfileFragment.kt
+++ app/src/main/java/com/example/traders/profile/UserProfileFragment.kt
@@ -7,15 +7,16 @@ import android.view.ViewGroup
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import com.example.traders.R
+import com.example.traders.databinding.FragmentUserProfileBinding
 
 class UserProfileFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        AlertDialog.Builder(requireContext()).setView(1).create()
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_user_profile, container, false)
+    ): View {
+        val binding = FragmentUserProfileBinding.inflate(inflater, container, false)
+        
+        return binding.root
     }
 }
