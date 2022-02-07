49116210e1d807a78e7f0232ca3062db9558e60b
aretka <aretask2@gmail.com>
2022-02-07 11:58:58 +0200

    Change version of room

--- app/src/main/java/com/example/traders/database/CryptoDatabaseDao.kt
+++ app/src/main/java/com/example/traders/database/CryptoDatabaseDao.kt
@@ -1,11 +1,15 @@
 package com.example.traders.database
 
+import androidx.lifecycle.LiveData
 import androidx.room.*
 
 @Dao
 interface CryptoDatabaseDao {
     @Query("SELECT * FROM crypto")
     suspend fun getAllCrypto(): List<Crypto>
+
+    @Query("SELECT * FROM crypto")
+    fun getAllCryptoLive(): LiveData<List<Crypto>>
 
     @Query("SELECT * FROM crypto WHERE symbol=:symbol")
     suspend fun getCryptoBySymbol(symbol: String): Crypto?
