73f0a1b67fcc4c29d78171bf29ebc9bad029f182
aretka <aretask2@gmail.com>
2022-10-23 20:45:00 +0300

    Add new file

---
+++ new_file.cpp
@@ -0,0 +1,5 @@
+//
+// Created by aretas on 2022-10-23.
+//
+
+#include "new_file.h"
---
+++ new_file.h
@@ -0,0 +1,16 @@
+//
+// Created by aretas on 2022-10-23.
+//
+
+#ifndef TRADERS_NEW_FILE_H
+#define TRADERS_NEW_FILE_H
+
+
+
+class new_file {
+
+};
+
+
+
+#endif //TRADERS_NEW_FILE_H
